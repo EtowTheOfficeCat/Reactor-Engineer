275990c8d6efc256e4dc44a2d5cdb8d4594bd5f0
EtowTheOffieCat <mabonp@gmail.com>
2019-07-27 09:21:07 +0200

    Erster Commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3((transform.position.x), Mathf.Clamp(transform.position.y, 0.95f, 1f), (transform.position.z));
+    }
+}
+
---
+++ Assets/_VRHands/Scripts/HandAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandAnimator : MonoBehaviour
+{
+    public SteamVR_Action_Single grabAction = null;
+
+    private Animator Animator = null;
+    private SteamVR_Behaviour_Pose pose = null;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+        pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+
+        grabAction[pose.inputSource].onChange += Grab;
+    }
+
+    private void OnDestroy()
+    {
+        grabAction[pose.inputSource].onChange -= Grab;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Animator.SetBool("Point", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Animator.SetBool("Point", false);
+    }
+
+    private void Grab(SteamVR_Action_Single action, SteamVR_Input_Sources source, float axis, float delta)
+    {
+        Animator.SetFloat("GrabBlend", axis);
+    }
+}
